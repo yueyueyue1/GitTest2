cd558c7bc82af88150de5dec3c4430c1d49ec5c8
jihaoyue <ji18610171109@163.com>
2022-09-26 11:32:42 +0800

    gitee 1

--- src/main/java/com/jihaoyue/git/gitTest.java
+++ src/main/java/com/jihaoyue/git/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
         System.out.println("hello hotfix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee push test");
     }
 }
